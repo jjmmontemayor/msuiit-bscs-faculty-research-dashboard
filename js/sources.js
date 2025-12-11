9c6eb957ea88f3ed9143508182fcdbe83d175b2c
Jennifer Joyce Montemayor <3021098+jjmmontemayor@users.noreply.github.com>
2025-12-11 18:01:54 +0800

    Add CSV_SOURCES object with faculty and yearly URLs

---
+++ js/sources.js
@@ -0,0 +1,5 @@
+// sources.js
+const CSV_SOURCES = {
+  faculty: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-jDPbQ-ZCkBK8_1igyWkHd3ynUPHh5l8eI6KmD-qhpu9ZeYTqYV11X7NQ82NbYyEwvSiTzGVD6-3L/pub?gid=5470856&single=true&output=csv",
+  yearly:  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-jDPbQ-ZCkBK8_1igyWkHd3ynUPHh5l8eI6KmD-qhpu9ZeYTqYV11X7NQ82NbYyEwvSiTzGVD6-3L/pub?gid=743932525&single=true&output=csv"
+};
