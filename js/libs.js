5456508146c6391fabaf90d956a6055e7c62da69
Jennifer Joyce M. Montemayor <3021098+jjmmontemayor@users.noreply.github.com>
2025-12-14 23:57:18 +0800

    refactor dashboard

--- js/libs.js
+++ js/libs.js
@@ -1,13 +1,30 @@
 const scripts = [
+  // jQuery (must be first)
   "https://code.jquery.com/jquery-3.7.1.min.js",
-  "https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js",
+
+  // PapaParse (CSV loader)
+  "https://cdn.jsdelivr.net/npm/papaparse@5.4.1/papaparse.min.js",
+
+  // Chart.js
   "https://cdn.jsdelivr.net/npm/chart.js",
-  "https://cdn.jsdelivr.net/npm/papaparse@5.4.1/papaparse.min.js"
+
+  // DataTables core
+  "https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js",
+
+  // DataTables Buttons
+  "https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js",
+
+  // Excel / CSV export dependencies
+  "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js",
+  "https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"
 ];
 
-scripts.forEach(src => {
-  const s = document.createElement('script');
-  s.src = src;
-  s.defer = true;
-  document.head.appendChild(s);
-});
+(function loadScriptsSequentially(index = 0) {
+  if (index >= scripts.length) return;
+
+  const script = document.createElement("script");
+  script.src = scripts[index];
+  script.defer = true;
+  script.onload = () => loadScriptsSequentially(index + 1);
+  document.head.appendChild(script);
+})();
--- js/sources.js
+++ js/sources.js
@@ -5,5 +5,6 @@ const CSV_SOURCES = {
   internals: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-jDPbQ-ZCkBK8_1igyWkHd3ynUPHh5l8eI6KmD-qhpu9ZeYTqYV11X7NQ82NbYyEwvSiTzGVD6-3L/pub?gid=0&single=true&output=csv",
   externals: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-jDPbQ-ZCkBK8_1igyWkHd3ynUPHh5l8eI6KmD-qhpu9ZeYTqYV11X7NQ82NbYyEwvSiTzGVD6-3L/pub?gid=191153071&single=true&output=csv",
   conferences: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-jDPbQ-ZCkBK8_1igyWkHd3ynUPHh5l8eI6KmD-qhpu9ZeYTqYV11X7NQ82NbYyEwvSiTzGVD6-3L/pub?gid=1454250764&single=true&output=csv",
-  publications: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-jDPbQ-ZCkBK8_1igyWkHd3ynUPHh5l8eI6KmD-qhpu9ZeYTqYV11X7NQ82NbYyEwvSiTzGVD6-3L/pub?gid=797482489&single=true&output=csv"
+  publications: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-jDPbQ-ZCkBK8_1igyWkHd3ynUPHh5l8eI6KmD-qhpu9ZeYTqYV11X7NQ82NbYyEwvSiTzGVD6-3L/pub?gid=797482489&single=true&output=csv",
+  creatives: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT8aFfrkFy48bj6BQ1Gf15BUbn-dRk1m1Suos0kFLs40tNq7l8Loh6MTbId1mCZtg/pub?gid=1635845573&single=true&output=csv"
 };
